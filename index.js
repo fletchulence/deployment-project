44823eb70f1f6cb062f6536facb1b68e33bab7b0
david fletcher <davidfletcheraaron@gmail.com>
2021-12-02 19:41:32 -0800

    ttrying to push to heroku

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 const server = express()
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/api/users', (req, res) =>{
    res.json([
@@ -9,6 +12,10 @@ server.get('/api/users', (req, res) =>{
       {id: 2, username: 'ear'},
       {id: 3, username: 'nose'},
    ]);
+})
+
+server.get('*', (req, res)=>{
+   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 // const port = 9000 //! heroku doesnt like this one
